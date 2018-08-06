7ae9ee91949dd8cb0911de3bc7fe6506a57e926a
Alasdair McLeay <penx@Alasdairs-MacBook-Pro.local>
2018-08-06 15:27:28 +0100

    use zsh to install nvm

--- node.sh
+++ node.sh
@@ -1,6 +1,6 @@
 # nvm
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh
 nvm install 6
 nvm install 8
 nvm use 8
