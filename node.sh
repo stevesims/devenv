10b8fa1b2824adaa8a79ca10a572a3b8442d39a7
Alasdair McLeay <penx@Alasdairs-MacBook-Pro.local>
2018-08-06 15:08:20 +0100

    node n -> nvm

--- node.sh
+++ node.sh
@@ -1,8 +1,16 @@
+# nvm
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm install 6
+nvm install 8
+nvm use 8
+
+# n
 npm install -g n
 
-n 6
-n 8
-# previous command installs, now call again to use:
-n 8
+# n 6
+# n 8
+# # previous command installs, now call again to use:
+# n 8
 
 npm install babel-cli depcheck source-map-explorer -g
